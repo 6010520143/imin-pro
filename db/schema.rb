4514e4ff391112eb421d9394f04edf798d4ad6f0
Ubuntu <ubuntu@ip-172-31-20-13.us-east-2.compute.internal>
2019-11-21 17:24:19 +0000

    Added  User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121165135) do
+ActiveRecord::Schema.define(version: 20191121170309) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20191121165135) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "phone"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
